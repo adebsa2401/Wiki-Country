fcb2aa1bcfcd2363e2fb4f0df7fddb6002df46c0
Ben Sales <adebsa2401@gmail.com>
2023-01-01 18:07:24 +0100

    feat: Styling the filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FaGreaterThanEqual, FaLessThanEqual } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import { filterCountries } from '../redux/countries/filter';
+import '../styles/Filter.css';
 
 export default function Filter() {
   const { limit, ge } = useSelector((state) => state.countriesFilter);
@@ -11,17 +12,20 @@ export default function Filter() {
     <div className="filter-container">
       <span>Population</span>
       <button
+        className="filter-sign-button"
         type="button"
         onClick={() => dispatch(filterCountries(limit, !ge))}
       >
         { ge ? <FaGreaterThanEqual /> : <FaLessThanEqual /> }
       </button>
       <input
+        className="filter-input"
         type="number"
-        value={limit}
+        value={limit / 1000}
         min={0}
-        onChange={(event) => dispatch(filterCountries(event.target.value, ge))}
+        onChange={(event) => dispatch(filterCountries(event.target.value * 1000, ge))}
       />
+      <span>000</span>
     </div>
   );
 }
